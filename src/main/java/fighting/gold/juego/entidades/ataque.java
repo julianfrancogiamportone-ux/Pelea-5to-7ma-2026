25089d711fc654f8f40e41ab5af9a8adb0e0939c
Nacho <ignaciosigampamz@gmail.com>
2026-04-23 09:35:35 -0300

    entidades

--- src/main/java/fighting/gold/juego/entidades/ataque.java
+++ src/main/java/fighting/gold/juego/entidades/ataque.java
@@ -1,44 +1,54 @@
 package fighting.gold.juego.entidades;
 
-import jakarta.persistence.*; 
+import jakarta.persistence.*;
+import java.util.List;
+
 @Entity
 public class ataque {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String nombre;
-    private int daniobase;
-    private int costoenergia;
-    public ataque(int id, String nombre, int daniobase, int costoenergia) {
+    int costoenergia;
+    int daniobase;
+
+    public ataque(int id, String nombre, int costoenergia, int daniobase) {
         this.id = id;
         this.nombre = nombre;
-        this.daniobase = daniobase;
         this.costoenergia = costoenergia;
+        this.daniobase = daniobase;
     }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public String getNombre() {
         return nombre;
     }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-    public int getDaniobase() {
-        return daniobase;
-    }
-    public void setDaniobase(int daniobase) {
-        this.daniobase = daniobase;
-    }
+
     public int getCostoenergia() {
         return costoenergia;
     }
+
     public void setCostoenergia(int costoenergia) {
         this.costoenergia = costoenergia;
     }
 
-    
+    public int getDaniobase() {
+        return daniobase;
+    }
+
+    public void setDaniobase(int daniobase) {
+        this.daniobase = daniobase;
+    }
+
 }
--- src/main/java/fighting/gold/juego/entidades/peleador.java
+++ src/main/java/fighting/gold/juego/entidades/peleador.java
@@ -1,92 +1,97 @@
 package fighting.gold.juego.entidades;
 
 import jakarta.persistence.*;
+import java.util.List;
+
 @Entity
 public class peleador {
+    private String nombre;
+    private int id;
+    private int puntosdevida;
+    private int energia;
+    private float defensabase;
+    private arma armaequipada;
+    private List inventario;
+    private List habilidades;
+
+    public peleador(String nombre, int id, int puntosdevida, int energia, float defensabase, arma armaequipada,
+            List inventario, List habilidades) {
+        this.nombre = nombre;
+        this.id = id;
+        this.puntosdevida = puntosdevida;
+        this.energia = energia;
+        this.defensabase = defensabase;
+        this.armaequipada = armaequipada;
+        this.inventario = inventario;
+        this.habilidades = habilidades;
+
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-    private arma inventario[];
-    private ataque habilidades[];
-    private int energia;
-    private String nombre;
-    private int vida;
-    private arma arma;
-    private float defensa;
-
-    public peleador(String nombre, int vida, arma arma, ataque habilidades[],int id, int energia, arma inventario[], float defensa) {
-        this.nombre = nombre;
-        this.vida = vida;
-        this.arma = arma;
-        this.habilidades = habilidades;
-        this.id = id;
-        this.energia = energia;
-        this.inventario = inventario;
-        this.defensa = defensa;
-    }
-    public int getId() {
+    public int getid() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setid(int id) {
         this.id = id;
     }
 
-    public arma[] getInventario() {
-        return inventario;
-    }
-
-    public void setInventario(arma[] inventario) {
-        this.inventario = inventario;
-    }
-
-    public ataque[] getHabilidades() {
-        return habilidades;
-    }
-
-    public void setHabilidades(ataque[] habilidades) {
-        this.habilidades = habilidades;
-    }
-
-    public int getEnergia() {
-        return energia;
-    }
-
-    public void setEnergia(int energia) {
-        this.energia = energia;
-    }
-
-    public String getNombre() {
+    public String getnombre() {
         return nombre;
     }
 
-    public void setNombre(String nombre) {
+    public void setnombre(String nombre) {
         this.nombre = nombre;
     }
 
-    public int getVida() {
-        return vida;
+    public int getpuntosdevida() {
+        return puntosdevida;
     }
 
-    public void setVida(int vida) {
-        this.vida = vida;
+    public void setpuntosdevida(int puntosdevida) {
+        this.puntosdevida = puntosdevida;
     }
 
-    public arma getArma() {
-        return arma;
+    public int energia() {
+        return energia;
     }
 
-    public void setArma(arma arma) {
-        this.arma = arma;
+    public void energia(int energia) {
+        this.energia = energia;
+
     }
 
-    public float getDefensa() {
-        return defensa;
+    public float getDefensabase() {
+        return defensabase;
     }
 
-    public void setDefensa(float defensa) {
-        this.defensa = defensa;
+    public void setDefensabase(float defensabase) {
+        this.defensabase = defensabase;
     }
 
-    
+    public arma getArmaequipada() {
+        return armaequipada;
+    }
+
+    public void setArmaequipada(arma armaequipada) {
+        this.armaequipada = armaequipada;
+    }
+
+    public List getInventario() {
+        return inventario;
+    }
+
+    public void setInventario(List inventario) {
+        this.inventario = inventario;
+    }
+
+    public List getHabilidades() {
+        return habilidades;
+    }
+
+    public void setHabilidades(List habilidades) {
+        this.habilidades = habilidades;
+    }
+
 }
